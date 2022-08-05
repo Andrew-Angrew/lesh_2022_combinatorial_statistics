b0df1d2bad4392d0240f1ce719a01867fc153613
Andrew Ploskonosov <Andrew-Angrew@yandex.ru>
2022-08-05 12:32:26 +0300

    update clean script

--- clean.sh
+++ clean.sh
@@ -1,11 +1,11 @@
-rm build/*.aux build/*.log build/*.synctex.gz
+rm -f build/*.aux build/*.log build/*.synctex.gz
 
-for dest_dir in "statistics_program", "basic_probability_problems"
+for dest_dir in "statistics_program" "basic_probability_problems"
 do
 	for f in $(ls ${dest_dir}/*.pdf)
 	do
 		f_name=$(basename "${f}")
-		(cmp "build/${f_name}" "${dest_dir}/${f_name}" > dev/null) || (cp "build/${f_name}" "${dest_dir}/${f_name}" && echo "${f_name} moved")
+		(cmp "build/${f_name}" "${dest_dir}/${f_name}" > /dev/null) || (cp "build/${f_name}" "${dest_dir}/${f_name}" && echo "${f_name} moved")
 	done
 done
 
